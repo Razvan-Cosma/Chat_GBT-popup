9a6a7205b374862815d7e594da2a60135ad4310c
Dpescar <pescar_diana@yahoo.com>
2024-10-29 15:44:53 +0200

    main

---
+++ components/chat-gbt.js
@@ -0,0 +1,7 @@
+document.querySelector('.chat-gpt-button').addEventListener('click', function() {
+    document.querySelector('.chat-gpt-menu').style.display = 'block';
+});
+
+document.querySelector('.chat-gpt-close').addEventListener('click', function() {
+    document.querySelector('.chat-gpt-menu').style.display = 'none';
+});
